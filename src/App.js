7c4fc2e857999a99fe6c5bb09673d05facbf1c7e
lavdimPireva <44246519+lavdimPireva@users.noreply.github.com>
2021-08-24 23:58:32 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Home from "./Home.js";
+import Header from "./Header.js";
+import Footer from "./Footer.js";
+import SearchPage from "./SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
